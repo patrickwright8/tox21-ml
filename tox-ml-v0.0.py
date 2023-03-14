b3ddf5f96553759aa8ba4579a1992983888bb7d1
patrickwright8 <46975870+patrickwright8@users.noreply.github.com>
2023-03-14 17:49:52 -0400

    Added tox-ml v0.0, full training data

---
+++ tox-ml-v0.0.py
@@ -0,0 +1,10 @@
+# %% Imports
+import chemprop
+import pandas as pd
+import numpy as np 
+
+from rdkit import Chem
+from rdkit.Chem.Draw import IPythonConsole #Needed to show molecules
+IPythonConsole.molSize = (350, 350)   # Change image size
+IPythonConsole.ipython_useSVG = True  # Change output to SVG
+
