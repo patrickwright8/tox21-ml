efdebfd76614416a3ecda98c827fae797eae5937
patrickwright8 <46975870+patrickwright8@users.noreply.github.com>
2023-04-02 08:55:03 -0400

    Added 10-fold CV, scaffold split

--- tox-ml-v0.0.py
+++ tox-ml-v0.0.py
@@ -8,12 +8,19 @@ IPythonConsole.ipython_useSVG = True  # Change output to SVG
     
 # %% Hyperparameter optimization
 
+# NOTE:
+    # For scafold split to work, you must change "np.float" in 
+    # C:\ProgramData\Anaconda3\envs\chemprop\Lib\site-packages\chemprop\data
+    # to "float" or "np.float64"
+
 arguments = [
     '--data_path', 'C:/projects/tox21-ml/data/combined_tox21.csv',
     '--dataset_type', 'classification',
     '--save_dir', 'C:/projects/tox21-ml/ckpts',
     '--gpu', '0',
     '--batch_size', '50',
+    '--num_folds', '10',
+    '--split_type', 'scaffold_balanced',
     '--smiles_columns', 'Smiles',
     '--target_columns', 'nr-ahr', 'nr-ar-lbd', 'nr-ar', 'nr-aromatase',\
         'nr-er-lbd', 'nr-er', 'nr-ppar-gamma', 'sr-are', 'sr-atad5', 'sr-hse',\
