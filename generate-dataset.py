75926ee32e677beca3901966442b8570418436d0
patrickwright8 <ptw804@gmail.com>
2023-04-08 10:12:30 -0400

    Updated paths for cleaning dataset

--- generate-dataset.py
+++ generate-dataset.py
@@ -4,16 +4,19 @@ import os
 
 from rdkit.Chem.MolStandardize import rdMolStandardize
 from rdkit import Chem
+from rdkit.Chem.Draw import IPythonConsole #Needed to show molecules
+IPythonConsole.molSize = (350, 350)   # Change image size
+IPythonConsole.ipython_useSVG = True  # Change output to SVG
 
-root = 'C:/projects/tox21-ml' # Eventually change to os.getcwd()
-datafolder = root + '/data/'
+root = os.getcwd() 
+datafolder = os.path.join(root, 'projects\\tox21-ml\\data')
 filenames = os.listdir(datafolder)
 filenames = [i for i in filenames if i.count('.smiles')>0]
 
 dfs = []
 smiles = np.nan
 for file in filenames:
-    temp_filepath = datafolder + file
+    temp_filepath = os.path.join(datafolder, file)
     assay_name = file.replace('.smiles','')
     temp_df = pd.read_csv(temp_filepath, sep='\s+|\t', engine='python',
                           header=None,
@@ -48,13 +51,15 @@ for count, sm in enumerate(smiles):
         smiles[count] = np.nan
         continue
     largest_mol = largest_Fragment.choose(m)
-    new_sm = Chem.MolToSmiles(largest_mol)
+    new_sm = Chem.MolToSmiles(largest_mol) # Canonical smiles
     smiles[count] = new_sm
 smiles = pd.Series(smiles, name='Smiles')
 df.iloc[:,0] = smiles
 df = df.dropna(subset='Smiles').drop_duplicates().reset_index(drop=True)
 
-savename = datafolder + 'combined_tox21.csv'
+# %% Export
+
+savename = os.path.join(datafolder,'combined_tox21.csv')
 df.to_csv(savename, index=False)
 
 print('\nSuccessfully exported to CSV!\n')
