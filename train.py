7c70b85536f99965d42fdd9c41370fed085dbe32
patrickwright8 <ptw804@gmail.com>
2023-04-12 16:49:19 -0400

    Added results from 10-fold CV

--- train.py
+++ train.py
@@ -51,7 +51,7 @@ arguments = [
     '--gpu', '0',
     '--batch_size', '50',
     '--num_folds', '10',
-    '--ensemble_size', '10'
+    '--ensemble_size', '10',
     '--features_generator', 'rdkit_2d_normalized',
     '--no_features_scaling',
     '--split_type', 'random',
