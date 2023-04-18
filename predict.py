606e530f1eba01885d0bfa8969d76e5b1b484a49
patrickwright8 <ptw804@gmail.com>
2023-04-18 18:49:48 -0400

    Ran predict.py, fixed some formatting

--- predict.py
+++ predict.py
@@ -1,4 +1,7 @@
 # %% Imports, I/O
+
+### Note: This program must be ran using the command line - I had trouble getting it to run using Spyder.
+
 import chemprop
 import os
 
@@ -7,10 +10,10 @@ from rdkit.Chem.Draw import IPythonConsole #Needed to show molecules
 IPythonConsole.molSize = (350, 350)   # Change image size
 IPythonConsole.ipython_useSVG = True  # Change output to SVG
 
-root = os.path.join(os.getcwd(), 'projects\\tox21-ml')
+root = os.getcwd()
 datapath = os.path.join(root, 'data\\test_data\\combined_test_set.csv')
 ensemble_savedir = os.path.join(root, 'ensemble_ckpts')
-prediction_path = os.path.join(root, 'predict\\predictions.csv')
+prediction_path = os.path.join(root, 'ensemble_preds\\predictions.csv')
 
 # %% Run ensemble predictions on test set
 
@@ -18,9 +21,11 @@ arguments = [
     '--test_path', datapath,
     '--preds_path', prediction_path,
     '--checkpoint_dir', ensemble_savedir,
+    '--num_workers', '0',
     '--gpu', '0',
     '--features_generator', 'rdkit_2d_normalized',
-    '--no_features_scaling'
+    '--no_features_scaling',
+    '--smiles_columns', 'smiles'
 ]
 
 args = chemprop.args.PredictArgs().parse_args(arguments)
