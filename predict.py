c6ecf7b3658d27ec875f2c0b7fd370fc462ad6f7
patrickwright8 <ptw804@gmail.com>
2023-04-19 18:47:00 -0400

    Code before re-training

--- predict.py
+++ predict.py
@@ -3,7 +3,9 @@
 ### Note: This program must be ran using the command line - I had trouble getting it to run using Spyder.
 
 import chemprop
+from chemprop.train.evaluate import evaluate_predictions
 import os
+import pandas as pd
 
 from rdkit import Chem
 from rdkit.Chem.Draw import IPythonConsole #Needed to show molecules
@@ -33,3 +35,32 @@ preds = chemprop.train.make_predictions(args=args)
 
 # %% Evaluates predictions by comparing to true values
 
+from sklearn.metrics import average_precision_score, precision_recall_curve
+from sklearn.metrics import auc, plot_precision_recall_curve
+
+root = os.getcwd()
+prediction_path = os.path.join(root, 'ensemble_preds\\predictions.csv')
+formatted_datapath = os.path.join(root, 'data\\test_data\\formatted_combined_test_set.csv')
+
+predictions = pd.read_csv(prediction_path)
+targets = pd.read_csv(formatted_datapath)
+
+predictions_as_list = predictions.iloc[:,1:].values.tolist()
+targets_as_list = targets.iloc[:,1:].values.tolist()
+
+# Data to plot precision - recall curve
+precision, recall, thresholds = precision_recall_curve(targets_as_list, predictions_as_list)
+# Use AUC function to calculate the area under the curve of precision recall curve
+auc_precision_recall = auc(recall, precision)
+print(auc_precision_recall)
+
+'''
+evaluate_predictions(
+    preds=predictions_as_list,
+    targets=targets_as_list,
+    num_tasks=12,
+    dataset_type='classification',
+    metrics=['prc-auc']
+)
+'''
+# %%
