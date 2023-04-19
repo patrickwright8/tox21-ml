d35b35f6605b3b61660fa42070a2b693e9767e13
patrickwright8 <ptw804@gmail.com>
2023-04-19 18:52:56 -0400

    Modified train.py to test with separate set

--- train.py
+++ train.py
@@ -8,10 +8,11 @@ IPythonConsole.molSize = (350, 350)   # Change image size
 IPythonConsole.ipython_useSVG = True  # Change output to SVG
 
 
-root = os.path.join(os.getcwd(), 'projects\\tox21-ml')
+root = os.getcwd()
 datapath = os.path.join(root, 'data\\combined_tox21.csv')
 configpath = os.path.join(root, 'hyperopt_results\\best.json')
-ensemble_savedir = os.path.join(root, 'ensemble_ckpts')
+ensemble_savedir = os.path.join(root, 'ensemble_ckpts_with_testing')
+test_set_path = os.path.join(root, 'data\\test_data\\formatted_combined_test_set.csv')
     
 # %% Run 5-fold CV on an ensemble of 3 models using the winning hyperparameters
 
@@ -21,12 +22,13 @@ arguments = [
     '--save_dir', ensemble_savedir,
     '--gpu', '0',
     '--batch_size', '50',
-    '--num_folds', '6',
-    '--ensemble_size', '3',
+    '--num_folds', '10',
+    '--ensemble_size', '10',
     '--features_generator', 'rdkit_2d_normalized',
     '--no_features_scaling',
     '--split_type', 'random',
-    '--split_size', '.95', '.05', '0',
+    '--split_size', '.9', '.1', '0',
+    '--separate_test_set', test_set_path,
     '--config_path', configpath,
     '--smiles_columns', 'Smiles',
     '--target_columns', 'nr-ahr', 'nr-ar-lbd', 'nr-ar', 'nr-aromatase',\
