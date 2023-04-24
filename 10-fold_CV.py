6dc780adb12cca00bc91698e06300f2098085257
patrickwright8 <ptw804@gmail.com>
2023-04-24 16:40:47 -0400

    Modified to train winning ensemble

--- 10-fold_CV.py
+++ 10-fold_CV.py
@@ -8,12 +8,12 @@ IPythonConsole.molSize = (350, 350)   # Change image size
 IPythonConsole.ipython_useSVG = True  # Change output to SVG
 
 
-root = os.path.join(os.getcwd(), 'projects\\tox21-ml')
+root = os.getcwd()
 datapath = os.path.join(root, 'data\\combined_tox21.csv')
-savedir = os.path.join(root, 'train_ckpts')
+savedir = os.path.join(root, '10-fold_CV_90_split')
 configpath = os.path.join(root, 'hyperopt\\best.json')
     
-# %% Perform 10-fold CV on single model
+# %% Perform 10-fold CV on ensemble
 
 # NOTE:
     # For scafold split to work, you must change "np.float" in 
@@ -27,34 +27,11 @@ arguments = [
     '--gpu', '0',
     '--batch_size', '50',
     '--num_folds', '10',
+    '--ensemble_size', '20',
     '--features_generator', 'rdkit_2d_normalized',
     '--no_features_scaling',
     '--split_type', 'random',
-    '--config_path', configpath,
-    '--smiles_columns', 'Smiles',
-    '--target_columns', 'nr-ahr', 'nr-ar-lbd', 'nr-ar', 'nr-aromatase',\
-        'nr-er-lbd', 'nr-er', 'nr-ppar-gamma', 'sr-are', 'sr-atad5', 'sr-hse',\
-            'sr-mmp', 'sr-p53',
-]
-
-args = chemprop.args.TrainArgs().parse_args(arguments)
-mean_score, std_score = chemprop.train.cross_validate(args=args, train_func=chemprop.train.run_training)
-
-# %% Run 10-fold CV on an ensemble of 10 models using the winning hyperparameters
-
-ensemble_savedir = os.path.join(root, 'ensemble_ckpts')
-
-arguments = [
-    '--data_path', datapath,
-    '--dataset_type', 'classification',
-    '--save_dir', ensemble_savedir,
-    '--gpu', '0',
-    '--batch_size', '50',
-    '--num_folds', '10',
-    '--ensemble_size', '10',
-    '--features_generator', 'rdkit_2d_normalized',
-    '--no_features_scaling',
-    '--split_type', 'random',
+    '--split_sizes', '.9 .05 .05',
     '--config_path', configpath,
     '--smiles_columns', 'Smiles',
     '--target_columns', 'nr-ahr', 'nr-ar-lbd', 'nr-ar', 'nr-aromatase',\
