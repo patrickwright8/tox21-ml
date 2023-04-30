351748ca5f0aa2de1aac337a21d759d34af304dd
patrickwright8 <ptw804@gmail.com>
2023-04-30 17:20:14 -0400

    Modified to only export test set

--- generate-test-dataset.py
+++ generate-test-dataset.py
@@ -13,13 +13,11 @@ IPythonConsole.ipython_useSVG = True  # Change output to SVG
 root = os.getcwd() 
 test_datapath = os.path.join(root, 'data\\test_data')
 
-eval_results_path = os.path.join(test_datapath, 'tox21_eval_dataset_results.csv')
-eval_smiles_path = os.path.join(test_datapath, 'tox21_eval_dataset_smiles.csv')
 test_sdf_path = os.path.join(test_datapath, 'tox21_test_dataset_results.txt')
 
 train_set_path = os.path.join(root, 'data\\combined_tox21.csv')
 
-export_path = os.path.join(test_datapath, 'combined_test_set.csv')
+export_path = os.path.join(test_datapath, 'test_set.csv')
 
 # Defines RDKit fragment cleaner
 largest_Fragment = rdMolStandardize.LargestFragmentChooser()
@@ -63,63 +61,18 @@ test_smiles_df = pd.DataFrame(test_smiles, columns=['Smiles'])
 test_dfs_uncombined = [test_smiles_df, test_results_df]
 test_set = pd.concat(test_dfs_uncombined, axis=1).drop_duplicates(subset=['Smiles']).reset_index(drop=True)
 
-# %% Generate dataframe with eval smiles and assay results
-
-# Read eval smiles from CSV
-eval_smiles = pd.read_csv(eval_smiles_path, names=['Smiles', 'Sample ID'], delimiter='\t', header=0)
-
-df = pd.read_csv(eval_results_path, delimiter='\t').replace('x', np.nan)
-
-# Here, I need to re-arrange the order of the assay results columns 
-# because they are not in the same order as the training set. 
-
-# Train set order: nr-ahr,nr-ar-lbd,nr-ar,nr-aromatase,nr-er-lbd,
-#   nr-er,nr-ppar-gamma,sr-are,sr-atad5,sr-hse,sr-mmp,sr-p53
-
-# Eval set order: 'NR-AhR', 'NR-AR', 'NR-AR-LBD', 'NR-Aromatase', 'NR-ER',
-#       'NR-ER-LBD', 'NR-PPAR-gamma', 'SR-ARE', 'SR-ATAD5', 'SR-HSE', 'SR-MMP',
-#       'SR-p53'
-
-# Note that I make the column "SAMPLE ID" the 0th column for the merge at the 
-# end of this code block.
-
-eval_results_columns = [df.iloc[:,0],df.iloc[:,1],df.iloc[:,3],df.iloc[:,2],df.iloc[:,4],
-                        df.iloc[:,6],df.iloc[:,5],df.iloc[:,7],df.iloc[:,8],
-                        df.iloc[:,9],df.iloc[:,10],df.iloc[:,11],df.iloc[:,12]]
-eval_results = pd.concat(eval_results_columns, axis=1)
-
-eval_smiles_list = list(eval_smiles.iloc[:,0])
-eval_smiles_cleaned = []
-
-for sm in eval_smiles_list:
-    m = Chem.MolFromSmiles(sm)
-    if m != None:
-        largest_mol = largest_Fragment.choose(m)
-        sm = Chem.MolToSmiles(largest_mol)
-        eval_smiles_cleaned.append(sm)
-    if m == None:
-        eval_smiles_cleaned.append(np.nan)
-
-eval_smiles.iloc[:,0] = eval_smiles_cleaned
-eval_smiles = eval_smiles.dropna(inplace=False).drop_duplicates(inplace=False).reset_index(drop=True)
-
-eval_set = pd.merge(eval_smiles, eval_results, on='Sample ID')
-eval_set = eval_set.drop(columns='Sample ID')
 # %% Combine dataframes into one test set! Export as CSV
 
-combined_test_set = pd.concat([test_set, eval_set]).reset_index(drop=True)
-combined_test_set = combined_test_set.drop_duplicates(subset=['Smiles'])
+test_set = test_set.drop_duplicates(subset=['Smiles'])
 
 # Generates set of all smiles present in training set
 train_set = set(pd.read_csv(train_set_path).iloc[:,0])
 
 # Removes rows in the test set containing smiles present in the training set
-test_set_filtered = combined_test_set[~combined_test_set['Smiles'].isin(train_set)]
+test_set_filtered = test_set[~test_set['Smiles'].isin(train_set)]
 
 # Shuffles test set and resets index
 final_test_set = test_set_filtered.sample(frac=1).reset_index(drop=True)
 
 # Exports to CSV
 final_test_set.to_csv(export_path, index=False)
-
-# %%
