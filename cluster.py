a3884d482fd814eb773a5745e56c0b4dc1a5de60
patrickwright8 <ptw804@gmail.com>
2023-05-01 21:23:28 -0400

    Updated to include test set

--- cluster.py
+++ cluster.py
@@ -14,24 +14,28 @@ sns.set(style='white', context='notebook', rc={'figure.figsize':(14,10)})
 root = os.getcwd()
 train_path = os.path.join(root, 'data\\train2vec.csv')
 eval_path = os.path.join(root, 'data\\test_data\\eval2vec.csv')
+test_path = os.path.join(root, 'data\\test_data\\test2vec.csv')
 
-# %% Combines train and eval sets into one df
+# %% Combines train, test, and eval sets into one df
 
 train = pd.read_csv(train_path)
 eval = pd.read_csv(eval_path)
+test = pd.read_csv(test_path)
 
 train = train.iloc[:,3:]
 eval = eval.iloc[:,3:]
+test = test.iloc[:,3:]
 
 train.insert(0, 'Type', 'Train')
 eval.insert(0, 'Type', 'Eval')
+test.insert(0, 'Type', 'Test')
 
-df = pd.concat([train, eval])
+df = pd.concat([train, test, eval])
 df_vecs = df.iloc[:,1:]
 
 # %% Trains UMAP on molecular vectors, then plots results
 
-reducer = umap.UMAP(n_neighbors=15, min_dist=.05)
+reducer = umap.UMAP()
 
 # Output of mol2vec is already "scaled", so no further input scaling is needed
 embedding = reducer.fit_transform(df_vecs)
@@ -39,7 +43,8 @@ embedding = reducer.fit_transform(df_vecs)
 plt.scatter(
     embedding[:, 0],
     embedding[:, 1],
-    c=[sns.color_palette()[x] for x in df.Type.map({"Train":0, "Eval":1})])
+    c=[sns.color_palette()[x] for x in df.Type.map({"Train":0, "Test":1, "Eval":2})])
 plt.gca().set_aspect('equal', 'datalim')
 plt.title('UMAP projection of Mol2Vec Data', fontsize=24)
+plt.legend(labels=['Train', 'Test', 'Eval'])
 # %%
